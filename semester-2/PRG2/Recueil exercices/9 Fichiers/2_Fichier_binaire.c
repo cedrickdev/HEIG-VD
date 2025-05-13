a93dd322c2d90dbb9da66b1a672db3c587951361
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-05-13 22:13:04 +0200

    Binary file handling OK

--- semester-2/PRG2/Recueil exercices/9 Fichiers/2_Fichier_binaire.c
+++ semester-2/PRG2/Recueil exercices/9 Fichiers/2_Fichier_binaire.c
@@ -2,31 +2,61 @@
 #include <stdbool.h>
 
 #define MAXLEN 256
+#define MAXSTOCK 80
 
+typedef struct {
+    char brand[MAXLEN];
+    char item[MAXLEN];
+    int qty;
+    float unit, total;
+} stockItem;
 
 int main(int argc, char const *argv[]) {
-    if (argc != 2) {
+    if (argc != 3) {
         printf("Usage: %s \n", argv[0]);
         return 1;
     }
 
-    FILE *fichier = fopen(argv[1], "r");
-    if (fichier == NULL) {
-        printf("Impossible de charger fichier %s", argv[1]);
+    FILE *file_in = fopen(argv[1], "rb");
+    if (!file_in) {
+        printf("[e] could not open %s\n", argv[1]);
         return 1;
     }
 
-    // start reading
-
-    char brand[MAXLEN];
-    char destination[MAXLEN];
-    int stock;
-    float unit, total;
+    stockItem stock[MAXSTOCK] = { 0 };
+    int i = 0;
 
 
-    while (fscanf(fichier, brand, destination, &stock, &unit, &total) == 5)
-        printf("reading %s,%s,%.2f,%d,%.2f", brand, destination, unit, stock, total);
-    fclose(fichier);
+    // start importing
+    while (fscanf(file_in, stock[i].brand, stock[i].item, &(stock[i].qty), &(stock[i].unit), &(stock[i].total)) != EOF) {
+        printf("reading %s,%s,%.2f,%d,%.2f", stock[i].brand, stock[i].item, stock[i].unit, stock[i].qty, stock[i].total);
+        i++;
+    }
+    fclose(file_in);
+
+
+    //create / overwrite the output file
+
+    FILE *file_out = fopen(argv[2], "wb");
+    if (!file_out) {
+        printf("[e] could not open %s\n", argv[2]);
+        return 1;
+    }
+
+
+    // DB size as header
+    if (fwrite(&i, sizeof(i), 1, file_out) != 1) {
+        printf("[e] could not write size of stock to %s\n", argv[2]);
+        return 1;
+    }
+
+    // DB rows
+    if (fwrite(stock, sizeof(stock[0]), i, file_out) != 1) {
+        printf("[e] could not write size of stock to %s\n", argv[2]);
+        return 1;
+    }
+
+    fclose(file_out);
 
     return 0;
 }
